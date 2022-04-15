e44d0fee2662fc84c944c3a18f146a8b18db05da
Raúl <ramaque01@gmail.com>
2022-04-15 18:42:19 +0100

    add screen's config

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import importAll from '../services/img';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const images = importAll(require.context('../services/img/profilePictures', false, /\.(png|jpe?g|svg|jpg)$/));
 
@@ -8,8 +10,17 @@ export default function Profile({ src, name, job }) {
     return <div className="picture">
         <img className="picture-img" src={images[src]} />
         <div className="picture-box">
-            <p className="picture-name">{name}</p>
-            <p className="picture-job">{job}</p>
+            <div className="picture-info">
+                <p className="picture-name">{name}</p>
+                <p className="picture-job">{job}</p>
+            </div>
+
+            <div className="picture-icon">
+                <FontAwesomeIcon icon={faTwitter} 
+                className="icon-tw"/>
+                <FontAwesomeIcon icon={faLinkedin}
+                className="icon-linkdn" />
+            </div>
         </div>
     </div>
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import ListOfProfiles from "../../components/listofprofiles";
 
 export default function Home() {
-    return <section className="flex h-screen w-full">
-        <ListOfProfiles  />
-    </section> 
+    return <div className="flex flex-col h-full 
+    w-full bg-gray-900 cursor-default">
+        <section className="px-16 mt-10">
+            <h2 className="text-white hover:text-blue-300 
+            cursor-pointer font-bold text-2xl">Meet our team</h2>
+
+            <p className="text-gray-400 text-xl mt-4">
+                Ornare sagittis, suspendisse in hendrerit quis.
+                Sed dui aliquet lectus sit pretium egestas vel mattis
+                neque.
+            </p>
+        </section>
+
+        <section className="my-8 mx-auto">
+            <ListOfProfiles  />
+        </section> 
+    </div>
 }
