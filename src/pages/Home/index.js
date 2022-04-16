c787a36ecf22efdd7ce62717408869a6388a5f64
Raúl <ramaque01@gmail.com>
2022-04-16 17:48:12 +0100

    add animation with frame-motion

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,46 @@
 import React from "react";
 import ListOfProfiles from "../../components/listofprofiles";
+import { motion } from "framer-motion";
 
 export default function Home() {
-    return <div className="flex flex-col h-full 
-    w-full bg-gray-900 cursor-default">
+    return <div className="flex flex-col h-full w-full bg-gray-900 cursor-default">
         <section className="px-16 mt-10">
-            <h2 className="text-white hover:text-blue-300 
-            cursor-pointer font-bold text-2xl">Meet our team</h2>
+            <motion.h2 className="text-white hover:text-blue-300 
+            cursor-pointer font-bold text-2xl"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+                duration: 4,
+                ease: 'easeInOut',
+                delay: 0.2,
+                type: 'spring'
+            }}>Meet our team</motion.h2>
 
-            <p className="text-gray-400 text-xl mt-4">
+            <motion.p className="text-gray-400 text-xl mt-4"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                  duration: 4,
+                  ease: 'easeInOut',
+                  delay: 0.2,
+                  type: 'spring'
+              }}>
                 Ornare sagittis, suspendisse in hendrerit quis.
                 Sed dui aliquet lectus sit pretium egestas vel mattis
                 neque.
-            </p>
+            </motion.p>
         </section>
 
-        <section className="my-8 mx-auto">
+        <motion.section className="flex card my-8 justify-center"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+            duration: 4,
+            ease: 'easeInOut',
+            delay: 0.2,
+            type: 'spring'
+        }}>
             <ListOfProfiles  />
-        </section> 
+        </motion.section> 
     </div>
 }
