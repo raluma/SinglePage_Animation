3e812c05b439605f522810b080099e52dfab3838
Raúl <ramaque01@gmail.com>
2022-04-16 17:46:54 +0100

    update tree

--- src/components/listofprofiles.js
+++ src/components/listofprofiles.js
@@ -4,13 +4,16 @@ import Profile from "./profile";
 
 export default function ListOfProfiles() {
     return (
-        <article className="card">
+        <>
             {dataProfiles.map((data, key) => {
                 return (
-                    <Profile key={key} src={data.img} name={data.name} job={data.job}/>
+                    <Profile key={key} 
+                    src={data.img} 
+                    name={data.name} 
+                    job={data.job}/>
                 )
             })
             }
-        </article>
+        </>
     )
 }
